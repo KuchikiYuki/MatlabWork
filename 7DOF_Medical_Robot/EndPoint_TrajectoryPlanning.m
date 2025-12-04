23ca4edf9d51020cc68640182b77657d09e1dd41
Turing-unpassed <ay52159wyh@163.com>
2025-12-04 11:54:56 +0800

    优化轨迹规划

--- 7DOF_Medical_Robot/EndPoint_TrajectoryPlanning.m
+++ 7DOF_Medical_Robot/EndPoint_TrajectoryPlanning.m
@@ -9,17 +9,26 @@ th1=45.0000;
 th2=41.7585;
 th3=117.2310;
 d4=0;
-th5=-90;
-th6=111.0104;
-th7=45; 
+th5=90.0000;
+th6=248.9896;
+th7=135;
 
 q_prev=[th1,th2,th3,d4,th5,th6,th7];
 
-q0 = [150,150,150,-90,111.0104,45];
-q1 = [450,450,450,44.6714,276.1133,276.1133];
+q0 = [150,150,150,0,0,0];
+q1 = [450,450,450,360,360,180];
 v_max = [1000,1000,1000,1000,1000,1000];
 A = [1000,1000,1000,1000,1000,1000];
 dt =0.01;
+
+% R = [1    0   0;
+%      0    1   0;
+%      0    0   1];
+% [alpha, beta, gamma] = ZYZ_EulerAngle_Solution(R)
+% R = [0    0   -1;
+%      1    0    0;
+%      0   -1    0];
+% [alpha, beta, gamma] = ZYZ_EulerAngle_Solution(R)
 
 x = 150:1:450;
 y = 150:1:450;
@@ -29,8 +38,7 @@ z = 150:1:450;
 
 step = length(q);
 
-t = 0:1:step-1;
-
+% t = 0:1:step-1;
 % figure(1);
 % plot(t,q(:,6));
 
@@ -39,14 +47,14 @@ pause(1);
 % view(0,0);
 % hold on;
 for i = 1:step
-    r11 = cosd(q(i,4)+180)*cosd(q(i,5))*cosd(q(i,6)+180) - sind(q(i,4)+180)*sind(q(i,6)+180);
-    r21 = sind(q(i,4)+180)*cosd(q(i,5))*cosd(q(i,6)+180) + cosd(q(i,4)+180)*sind(q(i,6)+180);
-    r31 = -sind(q(i,5))*cosd(q(i,6)+180);
-    r12 = -cosd(q(i,4)+180)*cosd(q(i,5))*sind(q(i,6)+180) - sind(q(i,4)+180)*cosd(q(i,6)+180);
-    r22 = -sind(q(i,4)+180)*cosd(q(i,5))*sind(q(i,6)+180) + cosd(q(i,4)+180)*cosd(q(i,6)+180);
-    r32 = sind(q(i,5))*sind(q(i,6)+180);
-    r13 = cosd(q(i,4)+180)*sind(q(i,5));
-    r23 = sind(q(i,4)+180)*sind(q(i,5));
+    r11 = cosd(q(i,4))*cosd(q(i,5))*cosd(q(i,6)) - sind(q(i,4))*sind(q(i,6));
+    r21 = sind(q(i,4))*cosd(q(i,5))*cosd(q(i,6)) + cosd(q(i,4))*sind(q(i,6));
+    r31 = -sind(q(i,5))*cosd(q(i,6));
+    r12 = -cosd(q(i,4))*cosd(q(i,5))*sind(q(i,6)) - sind(q(i,4))*cosd(q(i,6));
+    r22 = -sind(q(i,4))*cosd(q(i,5))*sind(q(i,6)) + cosd(q(i,4))*cosd(q(i,6));
+    r32 = sind(q(i,5))*sind(q(i,6));
+    r13 = cosd(q(i,4))*sind(q(i,5));
+    r23 = sind(q(i,4))*sind(q(i,5));
     r33 = cosd(q(i,5));
 
     T = [r11    r12    r13    q(i,1);
@@ -54,7 +62,7 @@ for i = 1:step
          r31    r32    r33    q(i,3);
           0      0      0        1  ];
     Q = Geometric_Inverse_Kinematics(Link,T);
-    [q_best,~,~] = Select_Optimal_Solution(Q,q_prev)
+    [q_best,~,~] = Select_Optimal_Solution(Q,q_prev);
     if i < step
         Forward_kinematics(Link,q_best,1);
         plot3(x,y,z,'r','LineWidth',1);
--- 7DOF_Medical_Robot/Medical_7DOF_Robot.m
+++ 7DOF_Medical_Robot/Medical_7DOF_Robot.m
@@ -7,22 +7,22 @@ Link = MDH_Table_Build();
 Link = Transimation_Matrix_Build(Link);
 ToDegree = 180/pi;
 th1=45.0000; 
-th2=35.3079;
-th3=243.3542;
-d4=300.0000;
-th5=89.6729;
-th6=92.5486;
-th7=96.0638; 
+th2=41.7585;
+th3=117.2310;
+d4=0;
+th5=90.0000;
+th6=248.9896;
+th7=135; 
 
 Q=[th1,th2,th3,d4,th5,th6,th7];
 
 T = Forward_kinematics(Link,Q,0)
 
-T = [0,0,-1,450;
-     1,0,0,450;
-     0,-1,0,450;
-     0,0,0,1];
-Geometric_Inverse_Kinematics(Link,T)
+% T = [1,0,0,150;
+%      0,1,0,150;
+%      0,0,1,150;
+%      0,0,0,1];
+Geometric_Inverse_Kinematics(Link,T);
 
 
 
